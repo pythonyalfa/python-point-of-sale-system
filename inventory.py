b58cf84533542dfdee95efc0376d6e32e7525b36
brentaharris <85082559+brentaharris@users.noreply.github.com>
2021-06-08 21:31:02 -0400

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,6 @@
+inventory = {"bread": 2.50, "butter": 3.15, "milk": 4.53, "cola": 2.00,
+    "medicine": 2.50, "water": 1.50, "newspaper": 2.50, "book": 14.40,
+    "magazine": 4.75, "toy": 5, "juice": 3.75,
+    "vegetables": 1.05, "fruit": 1.75, "shirt": 5.00, 
+    "dress": 22.15, "pants": 20.50}
+
